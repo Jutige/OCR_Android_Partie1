73c395f56f86cc8150d008f6a25ead38c8440e52
Jutige <caludije@gmail.com>
2020-05-04 11:31:49 +0200

    Essai de commit depuis Android Studio

--- app/src/main/java/judicael/tige/mytopquiz/controleur/MainActivity.java
+++ app/src/main/java/judicael/tige/mytopquiz/controleur/MainActivity.java
@@ -32,6 +32,7 @@ public class  MainActivity extends AppCompatActivity {
     public static final String PREF_KEY_SCORE = "PREF_KEY_SCORE";
     public static final String PREF_KEY_FIRSTNAME = "PREF_KEY_FIRSTNAME";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
